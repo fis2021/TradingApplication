bae2715e227c950b0b334430b4e4c7a7337877e9
Poza Radu <poza.radu@gmail.com>
2021-04-27 14:25:57 +0300

    Created homepage GUI

---
+++ src/main/java/org/fis/ta/controllers/HomepageController.java
@@ -0,0 +1,18 @@
+package org.fis.ta.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+public class HomepageController {
+
+    @FXML
+    private Text greetingMessage;
+
+    @FXML
+    void logoutAction() {
+
+    }
+
+}
+
