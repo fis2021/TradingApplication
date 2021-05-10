d9bcb3ab8f79abbb50b8ea42b2d331397bbb2cdb
Poza Radu <poza.radu@gmail.com>
2021-05-10 14:32:27 +0300

    created the items table

--- src/main/java/org/fis/ta/controllers/SaleslistController.java
+++ src/main/java/org/fis/ta/controllers/SaleslistController.java
@@ -1,4 +1,33 @@
 package org.fis.ta.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import org.fis.ta.model.Item;
+
+import javax.swing.text.html.ImageView;
+
 public class SaleslistController {
+
+    @FXML
+    private TableView<Item> table;
+    @FXML
+    private TableColumn<Item, ImageView> photoColumn;
+
+    @FXML
+    private TableColumn<Item, String> nameColumn;
+
+    @FXML
+    private TableColumn<Item, String> priceColumn;
+
+
+    public void loadSaleslistPage(){
+        nameColumn=new TableColumn<>("Name");
+        priceColumn=new TableColumn<>("Price");
+        nameColumn.setMinWidth(200);
+        priceColumn.setMinWidth(100);
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+    }
 }
