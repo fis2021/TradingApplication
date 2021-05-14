d847b207491a95956ab998edfbbcd98711b0bd44
Nicu <nicu_usvat@yahoo.com>
2021-05-14 15:31:35 +0300

    Created text field for search box/TA-70

--- src/main/java/org/fis/ta/controllers/DisplayItemsPageController.java
+++ src/main/java/org/fis/ta/controllers/DisplayItemsPageController.java
@@ -5,10 +5,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableCell;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
@@ -36,6 +33,10 @@ public class DisplayItemsPageController {
 
     @FXML
     private TableView itemsTableView;
+
+    @FXML
+    private TextField filterField;
+
     @FXML
     private void initialize(){
         fillTable();
