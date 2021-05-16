9157119de264b9e11ced53c49a553865060f39e8
Poza Radu <poza.radu@gmail.com>
2021-05-16 14:32:54 +0300

    Tested manage item feature /TA-86

--- src/test/java/org/fis/ta/controllers/ManageItemTest.java
+++ src/test/java/org/fis/ta/controllers/ManageItemTest.java
@@ -18,13 +18,16 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.api.FxRobot;
+import org.testfx.assertions.api.Assertions;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.testfx.assertions.api.Assertions.assertThat;
 
 @ExtendWith({ApplicationExtension.class})
 class ManageItemTest {
@@ -156,14 +159,44 @@ class ManageItemTest {
 
         robot.clickOn("#editpagePriceField");
         robot.write("120");
-        assertThrows(PriceNotValidException.class, () -> {
-            robot.clickOn("#editpageEditButton");
-        });
 
-        //robot.clickOn("#editpagePriceField");
-       // robot.write(".00");
-       // robot.clickOn("#editpageDescriptionField");
-        //robot.clickOn("#editpageEditButton");
+        robot.clickOn("#editpageEditButton");
+        assertThat(robot.lookup("#editpageErrorMessage").queryText()).hasText("Please enter a valid price!");
+
+
+        robot.clickOn("#editpagePriceField");
+        robot.write(".00");
+        robot.clickOn("#editpageDescriptionField");
+        robot.write("1");
+        robot.clickOn("#editpageEditButton");
+    }
+
+    @Test
+    @DisplayName("Item deletes from the database")
+    void testDeleteItem(FxRobot robot) throws PhoneNumberNotValidException, EmptyFieldException, EmailNotValidException, PasswordDoesntMatchException, UsernameAlreadyExistsException, PriceNotValidException, NoFileSelectedException {
+        UserService.addUser(USERNAME,PASSWORD,CONFIRMPASSWORD,FIRSTNAME,LASTNAME,EMAIL,PHONENUMBER);
+        User user = UserService.getAllUsers().get(0);
+        ItemService.addItem(USERNAME,NAME,CATEGORY,DESCRIPTION,IMAGES,PRICE);
+        Item item = ItemService.getAllItems().get(0);
+
+        robot.clickOn("#loginUsername");
+        robot.write(USERNAME);
+        robot.clickOn("#loginPassword");
+        robot.write(PASSWORD);
+        robot.clickOn("#loginButton");
+
+
+        robot.clickOn("#homepageSalesList");
+        robot.type(KeyCode.UP);
+        robot.clickOn("#saleslistTable");
+        robot.clickOn("#itempageManageButton");
+        robot.clickOn("#managepageDeleteButton");
+        robot.clickOn("#noButton");
+        assertThat(ItemService.getAllItems()).size().isEqualTo(1);
+        robot.clickOn("#itempageManageButton");
+        robot.clickOn("#managepageDeleteButton");
+        robot.clickOn("#yesButton");
+        assertThat(ItemService.getAllItems()).isEmpty();
 
     }
 }
